71b3a25aed6ec06b7ecc9dd2bebceda698571e8c
filippow <34234269+filippow@users.noreply.github.com>
2018-01-10 18:47:55 +0300

    фыв

--- photo_site/js/main.js
+++ photo_site/js/main.js
@@ -5,8 +5,9 @@ var target = this.hash,
  $target = $(target);
  
 $('html, body').stop().animate({
- 'scrollTop': $target.offset().top
+ 'scrollTop': $target.offset().top-100
  }, 2000, 'swing', function () {
- window.location.hash = target;
+ window.location.hash = target-100;
  });
  });
+
